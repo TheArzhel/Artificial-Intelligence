b2f962225c7d59c2e60a94101a2be078ee5a1672
Andres Saladrigas <36264953+TheArzhel@users.noreply.github.com>
2019-11-02 18:13:21 +0100

    Add steeringFollowPath

--- kind of a Bussines/Assets/Scripts/Steering/SteeringFollowPath.cs
+++ kind of a Bussines/Assets/Scripts/Steering/SteeringFollowPath.cs
@@ -1,18 +1,69 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
+using BansheeGz.BGSpline.Components;
+using BansheeGz.BGSpline.Curve;
+
 
 public class SteeringFollowPath : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+
+
+    Move move;
+    SteeringSeek seek;
+    public BGCcMath curve;
+
+    public float ratio = 0.0f;
+    public float ratio_increment = 0.1f;
+    public float min_distance = 1.0f;
+    float current_ratio = 0.0f;
+
+    Vector3 ClosestPoint1;
+
+    // Use this for initialization
     void Start()
     {
-        
+        move = GetComponent<Move>();
+        seek = GetComponent<SteeringSeek>();
+        //curve = GetComponent<BGCcMath>();
+
+        Vector3 ClosestPoint = curve.CalcPositionByClosestPoint(transform.position, out current_ratio);
+        ClosestPoint1 = ClosestPoint;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Vector3.Distance(transform.position, ClosestPoint1) < min_distance)
+        {
+            seek.Steer(ClosestPoint1);
+        }
+        else
+        {
+
+            if (current_ratio >= 1.0f)
+            {
+                current_ratio = 0.0f;
+            }
+
+            current_ratio += (ratio_increment * Time.deltaTime);
+            Vector3 newpos = curve.CalcPositionByDistance(current_ratio);
+            transform.position = newpos;
+        }
+
+        // TODO 2: Check if the tank is close enough to the desired point
+        // If so, create a new point further ahead in the path
+    }
+
+    void OnDrawGizmosSelected()
+    {
+
+        if (isActiveAndEnabled)
+        {
+            // Display the explosion radius when selected
+            Gizmos.color = Color.green;
+            // Useful if you draw a sphere were on the closest point to the path
+        }
+
     }
 }
