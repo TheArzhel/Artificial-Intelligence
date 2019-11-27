e6e83aaa3169c01fa77d7f979e4b789fa68a3565
Adrián Font Romero <36204357+AdrianFR99@users.noreply.github.com>
2019-11-27 10:04:59 +0100

    Perception manager structure added

---
+++ kind of a Bussines/Assets/Scripts/PerceptionSystem/PerceptionManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+
+
+
+public class PerceptionEvent
+{
+
+    public enum sense {VISION};
+    public enum types {NEW,LOST};
+
+
+    public GameObject GO;
+    public sense Sense;
+    public types Type;
+
+
+
+}
+public class PerceptionManager : MonoBehaviour
+{
+     void EventPercieved(PerceptionEvent Event)
+    {
+        if (Event.Type == global::PerceptionEvent.types.NEW)
+        {
+            Debug.Log("GO Detected");
+
+
+
+
+        }
+        else if (Event.Type == global::PerceptionEvent.types.LOST)
+        {
+
+            Debug.Log("GO Lost");
+
+
+
+
+        }
+
+    }
+}
