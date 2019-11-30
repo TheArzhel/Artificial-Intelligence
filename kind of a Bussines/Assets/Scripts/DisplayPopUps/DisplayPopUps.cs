49e5a64989562d85f23ffb9d7e0b50145f7d0773
Adrián Font Romero <36204357+AdrianFR99@users.noreply.github.com>
2019-11-30 18:24:56 +0100

    Added PopUps

--- kind of a Bussines/Assets/Scripts/DisplayPopUps/DisplayPopUps.cs
+++ kind of a Bussines/Assets/Scripts/DisplayPopUps/DisplayPopUps.cs
@@ -1,24 +1,54 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.U2D;
+
+enum PopUp_Type { Inspect, Detected,Money, Food, Alcohol }
 
 public class DisplayPopUps : MonoBehaviour
 {
 
 
-    public Sprite[] PopUps;
 
+    public Vector3 offset = new Vector3(0,2,0);
+    public Vector3 SpriteScale = new Vector3(3, 3, 3);
+    public float DestroyTimePopUp = 3.0f;
 
+    [SerializeField]
+    private PopUp_Type current;
+
+    [SerializeField]
+    private SpriteAtlas PopAtlas;
+
+    private SpriteRenderer myrender;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
+        transform.localPosition += offset;
+        transform.localScale = SpriteScale;
+
+        myrender = GetComponent<SpriteRenderer>();
+
+        current = PopUp_Type.Inspect;
+        ChangeSprite();
+
+        Destroy(gameObject, DestroyTimePopUp);
+
     }
 
     // Update is called once per frame
-    void Update()
+  
+
+    void ChangeSprite()
     {
-        
+
+
+        //TODO:switch
+        myrender.sprite = PopAtlas.GetSprite(current.ToString());
+
+
+
     }
 }
---
+++ kind of a Bussines/Assets/Scripts/FeedBackTools/FeedBack.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+
+public class FeedBack : MonoBehaviour
+{
+
+
+    public GameObject SpritePref;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (SpritePref != null)
+            ShowPopUp();
+
+    }
+
+    void ShowPopUp()
+    {
+
+        Instantiate(SpritePref,transform.position,Quaternion.identity,transform);
+
+    }
+}
