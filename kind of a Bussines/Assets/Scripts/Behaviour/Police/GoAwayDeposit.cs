ae0bce1668b4e4d290f34a04e662743ad1d3cbbd
Andres Saladrigas <36264953+TheArzhel@users.noreply.github.com>
2019-12-10 22:12:46 +0100

    creating tree for police

---
+++ kind of a Bussines/Assets/Scripts/Behaviour/Police/GoAwayDeposit.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NodeCanvas.Framework;
+using BansheeGz.BGSpline.Components;
+using BansheeGz.BGSpline.Curve;
+
+
+public class GoAwayDesposit : ActionTask
+{
+    public BGCcMath Curve;
+
+    private BGCcMath CurrentCurve;
+
+    private bool ret = false;
+
+
+    Move move;
+    FollowCurve PathControl;
+
+    // Start is called before the first frame update
+    protected override void OnExecute()
+    {
+        ret = false;
+        CurrentCurve = null;
+        move = ownerAgent.gameObject.GetComponent<Move>();
+        move.finished = false;
+        PathControl = ownerAgent.gameObject.GetComponent<FollowCurve>();
+        Debug.Log("ret" + ret + CurrentCurve);
+
+    }
+
+    // Update is called once per frame
+    protected override void OnUpdate()
+    {
+
+
+
+        if (!ret)
+        {
+            if (CurrentCurve == null)
+            {
+                ret = ChooseCurve();
+                Debug.Log("choose curve ret " + ret + CurrentCurve);
+
+
+            }
+
+            Debug.Log(ret);
+            if (ret)
+            {
+
+                PathControl.SetCurve(CurrentCurve);
+
+                // move.finished = false;
+            }
+            else
+                EndAction(false);
+        }
+        else if (move.finished && ret)
+        {
+
+            EndAction(true);
+            Debug.Log("end " + CurrentCurve);
+        }
+
+    }
+
+    public bool ChooseCurve()
+    {
+        bool ret = false;
+
+        CurrentCurve = Curve;
+        ret = true;
+
+
+        return ret;
+    }
+}
---
+++ kind of a Bussines/Assets/Scripts/Behaviour/Police/GotoDeposit.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NodeCanvas.Framework;
+using BansheeGz.BGSpline.Components;
+using BansheeGz.BGSpline.Curve;
+
+
+public class GotoDeposit : ActionTask
+{
+
+    private BGCcMath CurrentCurve;
+
+    private bool ret = false;
+
+    private GameObject Kitchen;
+
+    Move move;
+    FollowCurve PathControl;
+
+    // Start is called before the first frame update
+    protected override void OnExecute()
+    {
+        ret = false;
+        CurrentCurve = null;
+        move = ownerAgent.gameObject.GetComponent<Move>();
+        move.finished = false;
+        PathControl = ownerAgent.gameObject.GetComponent<FollowCurve>();
+
+        //Debug.Log("ret" + ret + CurrentCurve);
+
+    }
+
+    // Update is called once per frame
+    protected override void OnUpdate()
+    {
+
+
+        if (!ret)
+        {
+            if (CurrentCurve == null)
+            {
+                FindCurve();
+                if (CurrentCurve != null)
+                { ret = true; }
+                //Debug.Log("choose curve ret " + ret + CurrentCurve);
+
+            }
+
+            Debug.Log(ret);
+            if (ret)
+            {
+
+                PathControl.SetCurve(CurrentCurve);
+
+                // move.finished = false;
+            }
+            else
+                EndAction(false);
+        }
+        else if (move.finished && ret)
+        {
+
+            EndAction(true);
+            //Debug.Log("end " + CurrentCurve);
+        }
+
+    }
+
+    //public bool ChooseCurve()
+    //{
+    //    bool ret=false;
+    //    //must choose random
+    //    int a= Random.Range(1, 3);
+    //    if (a <= 1)
+    //    {
+    //        CurrentCurve = Curve;
+    //        ret = true;
+    //    }
+    //    else if (a <= 2)
+    //    {
+    //        CurrentCurve = Curve1;
+    //        ret = true;
+    //    }
+    //    else
+    //    {
+    //        CurrentCurve = Curve2;
+    //        ret = true;
+
+    //    }
+
+
+
+    //    return ret;
+    //}
+
+    private void FindCurve()
+    {
+        Kitchen = GameObject.FindGameObjectWithTag("Kitchen");
+        if (Kitchen != null)
+            Debug.Log("exist");
+        KitchenScrip KitchenControler;
+        KitchenControler = Kitchen.GetComponent<KitchenScrip>();
+        if (Kitchen != null)
+            Debug.Log("exis2t");
+        CurrentCurve = KitchenControler.AskPath();
+    }
+}
