9bb38bd7e4aa2ac00cce2d09bd1c159afd937cb1
Adrián Font Romero <36204357+AdrianFR99@users.noreply.github.com>
2019-12-12 22:26:36 +0100

    Raycast hit to worker

---
+++ kind of a Bussines/Assets/Scripts/UI/CharacterActionUI.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterActionUI : MonoBehaviour
+{
+
+    public GameObject CharacterUIaction;
+    GameObject SelectedEntity;
+    bool TargetHit;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+
+            RaycastHit hit;
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.rigidbody.gameObject.CompareTag("Worker"))
+                {
+
+                    SelectedEntity = hit.rigidbody.gameObject;
+                    TargetHit = true;
+
+                }
+
+
+            }
+
+
+            if (TargetHit){
+
+                CharacterUIaction.SetActive(true);
+                
+
+
+
+
+            }
+                       
+        }
+
+
+    }
+}
