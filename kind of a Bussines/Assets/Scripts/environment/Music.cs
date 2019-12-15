d7bd87705bb2a197d92e862f92b46ed743560dac
Andres Saladrigas <36264953+TheArzhel@users.noreply.github.com>
2019-12-15 20:28:43 +0100

    audio up

---
+++ kind of a Bussines/Assets/Scripts/environment/Music.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Music : MonoBehaviour
+{
+    public AudioSource src;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        src.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
