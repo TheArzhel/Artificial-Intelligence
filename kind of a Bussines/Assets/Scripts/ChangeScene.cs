2e873bfcbea44fceac02fd97c2f24399608b3ba6
Adrián Font Romero <36204357+AdrianFR99@users.noreply.github.com>
2019-12-15 17:07:14 +0100

    Fade complete

--- kind of a Bussines/Assets/Scripts/ChangeScene.cs
+++ kind of a Bussines/Assets/Scripts/ChangeScene.cs
@@ -6,14 +6,39 @@ using UnityEngine.SceneManagement;
 public class ChangeScene : MonoBehaviour
 {
     // Start is called before the first frame update
-   
+
+
+    public Animator animator; 
+    private int levelToLoad;
+
+
+    
+
     public void LoadGameScene()
     {
-
         SceneManager.LoadScene("KindOfaBussines");
-
     }
 
 
+
+    public void FadeToNextLevel()
+    {
+
+        FadeTolevel(SceneManager.GetActiveScene().buildIndex+1);
+
+     }
+
+    public void FadeTolevel(int index)
+    {
+
+        levelToLoad = index;
+        animator.SetTrigger("FadeOut");
+        
+    }
+    public void OnFadeComplete()
+    { 
+        SceneManager.LoadScene(levelToLoad);
+
+    }
 
 }
